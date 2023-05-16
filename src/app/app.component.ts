bb5204a71517f12f13b2536ed33473b77f556cf5
zhao0235 <zhao0235@algonquinlive.com>
2023-05-16 15:33:55 -0400

    delete redundent package

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,24 +14,26 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.reactiveForm = new FormGroup({
+      // Group of form controls using FormGroup
       personalDetails: new FormGroup({
         firstname: new FormControl(null, [
           Validators.required,
-          this.noSpaceAllowed,
+          this.noSpaceAllowed, //Custom Validation
         ]),
         lastname: new FormControl(null, [
           Validators.required,
           this.noSpaceAllowed,
         ]),
-        email: new FormControl(null, [
-          Validators.required,
-          Validators.email,
-          this.emailNotAllowed,
-        ]),
+        email: new FormControl(
+          null,
+          [Validators.required, Validators.email],
+          this.emailNotAllowed
+        ),
       }),
       gender: new FormControl('male'), //set default value
       country: new FormControl('india'),
       hobbies: new FormControl(null),
+      // Group of form controls using FormArray
       skills: new FormArray([new FormControl(null, Validators.required)]),
     });
 
@@ -117,3 +119,9 @@ export class AppComponent implements OnInit {
     return response;
   }
 }
+
+// From Controls can be grouped in two ways:
+// 1. Using FormGroup
+// 2. Using FormArray
+
+//Async validator is used when need to sent HTTP request to the server to check if the data is valid
